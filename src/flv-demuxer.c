6295ef6cac383514a45d7c6cfa31f4c0ac4c4058
wangzhanqiang <wangzhanqiang@bytedance.com>
2018-11-30 14:46:27 +0800

    1

--- src/flv-demuxer.c
+++ src/flv-demuxer.c
@@ -11,7 +11,7 @@
 
 #define FLV_VIDEO_KEY_FRAME	1
 
-#define N_FLV_HEADER		9		// DataOffset included
+#define N_FLV_HEADER		9		//
 #define N_TAG_HEADER		11		// StreamID included
 #define N_TAG_SIZE			4		// previous tag size
 
